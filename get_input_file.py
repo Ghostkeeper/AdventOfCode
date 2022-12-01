89e840b32627a2d52fb46e43a430d7357cabb808
Ghostkeeper <rubend@tutanota.com>
2022-12-01 16:33:23 +0100

    Correct documentation of 1-indexed inputs

--- get_input_file.py
+++ get_input_file.py
@@ -5,8 +5,8 @@ import os.path
 def get_path(day, exercise, index = 0):
     """
     Get the file path towards an input file.
-    :param day: The day of the exercise (0-25)
-    :param exercise: The exercise index (0-1)
+    :param day: The day of the exercise (1-25)
+    :param exercise: The exercise index (1-2)
     :param index: If the exercise has multiple input files, the index to the file you want.
     :return: A path to a file.
     """
