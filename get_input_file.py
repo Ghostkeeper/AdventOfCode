d9236a9a93966506c1f7aa389c5e14a14a2fa65c
Ghostkeeper <rubend@tutanota.com>
2022-12-01 16:31:08 +0100

    Add a way to get the input of an assignment

---
+++ get_input_file.py
@@ -0,0 +1,18 @@
+# Advent of code 2022, by Ghostkeeper
+
+import os.path
+
+def get_path(day, exercise, index = 0):
+    """
+    Get the file path towards an input file.
+    :param day: The day of the exercise (0-25)
+    :param exercise: The exercise index (0-1)
+    :param index: If the exercise has multiple input files, the index to the file you want.
+    :return: A path to a file.
+    """
+    inputs_dir = os.path.join(os.path.dirname(__file__), "inputs")
+    result = os.path.join(inputs_dir, f"day{day}_{exercise}")
+    if index != 0:
+        result += f"_{index}"
+    result += ".txt"
+    return result
