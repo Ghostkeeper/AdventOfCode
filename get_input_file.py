06c68526717a2b30b61e0bf0ede5740f67e0b5f4
Ghostkeeper <rubend@tutanota.com>
2022-12-02 08:11:04 +0100

    Tab to indent

--- get_input_file.py
+++ get_input_file.py
@@ -3,16 +3,16 @@
 import os.path
 
 def get_path(day, exercise, index = 0):
-    """
-    Get the file path towards an input file.
-    :param day: The day of the exercise (1-25)
-    :param exercise: The exercise index (1-2)
-    :param index: If the exercise has multiple input files, the index to the file you want.
-    :return: A path to a file.
-    """
-    inputs_dir = os.path.join(os.path.dirname(__file__), "inputs")
-    result = os.path.join(inputs_dir, f"day{day}_{exercise}")
-    if index != 0:
-        result += f"_{index}"
-    result += ".txt"
-    return result
+	"""
+	Get the file path towards an input file.
+	:param day: The day of the exercise (1-25)
+	:param exercise: The exercise index (1-2)
+	:param index: If the exercise has multiple input files, the index to the file you want.
+	:return: A path to a file.
+	"""
+	inputs_dir = os.path.join(os.path.dirname(__file__), "inputs")
+	result = os.path.join(inputs_dir, f"day{day}_{exercise}")
+	if index != 0:
+		result += f"_{index}"
+	result += ".txt"
+	return result
