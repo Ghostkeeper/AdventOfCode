622998b567a4f0e05bad23da83cd1479b0274427
Ghostkeeper <rubend@tutanota.com>
2022-12-10 20:46:44 +0100

    Fix initial cycle count
    Stupid 1-ary counting.

--- day10_1.py
+++ day10_1.py
@@ -9,11 +9,12 @@ interesting_cycles = {20, 60, 100, 140, 180, 220}
 sum_interesting_signals = 0
 
 x = 1
-cycle = 0
+cycle = 1
 for command in commands:
 	if command == "noop":
 		if cycle in interesting_cycles:
 			sum_interesting_signals += x * cycle
+			print(f"Interesting {cycle}: {x} ({x * cycle}) {command}")
 		cycle += 1
 	else:
 		parts = command.split(" ")
@@ -21,7 +22,8 @@ for command in commands:
 		if cycle in interesting_cycles or cycle + 1 in interesting_cycles:
 			interesting_cycle = cycle if cycle in interesting_cycles else (cycle + 1)
 			sum_interesting_signals += x * interesting_cycle
+			print(f"Interesting {interesting_cycle}: {x} ({x * interesting_cycle}) {command}")
 		x += delta
 		cycle += 2
 
-print("The sum of the interestng signal strengths is:", sum_interesting_signals)
+print("The sum of the interesting signal strengths is:", sum_interesting_signals)
