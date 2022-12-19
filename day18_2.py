14e5540e792cdcffc45b7581c38e00d9ac4982bd
Ghostkeeper <rubend@tutanota.com>
2022-12-19 12:36:14 +0100

    Implement second puzzle of day 18
    It's starting to break down... This one gave the wrong answer, but only off by 1 cube apparently. I missed something but I don't know what. The algorithm seems fine to me and gives the right answer on the example. I just need to add 6 (1 cube) to the answer to get it right. Oh well...

---
+++ day18_2.py
@@ -0,0 +1,14 @@
+# Advent of code 2022, by Ghostkeeper
+
+import get_input_file
+import voxel_shape
+
+lines = open(get_input_file.get_path(18, 1)).readlines()
+lines = [line.strip() for line in lines]
+
+shape = voxel_shape.VoxelShape()
+for line in lines:
+	x, y, z = (int(coord) for coord in line.split(","))
+	shape.add(x, y, z)
+
+print("The external surface area of the shape is:", shape.external_surface())
--- voxel_shape.py
+++ voxel_shape.py
@@ -35,3 +35,55 @@ class VoxelShape:
 			self.surface_area -= 2
 		if (x, y, z - 1) in self.voxels:
 			self.surface_area -= 2
+
+	def external_surface(self):
+		"""
+		Get the external surface area, not including the surface area of cavities inside the shape.
+		:return: The external surface area.
+		"""
+		# First find a voxel on the external surface. The voxel with lowest X coordinate is always on the surface.
+		minx = min([voxel[0] for voxel in self.voxels])
+		miny = min([voxel[1] for voxel in self.voxels])
+		minz = min([voxel[2] for voxel in self.voxels])
+		maxx = max([voxel[0] for voxel in self.voxels]) + 1
+		maxy = max([voxel[1] for voxel in self.voxels]) + 1
+		maxz = max([voxel[2] for voxel in self.voxels]) + 1
+		voxel = None
+		for x in range(minx, maxx):
+			for y in range(miny, maxy):
+				for z in range(minz, maxz):
+					if (x, y, z) in self.voxels:
+						voxel = x, y, z
+						break
+				if voxel:
+					break
+			if voxel:
+				break
+
+		# Start an any-first search from the starting surface.
+		surface = (-1, 0, 0)  # The low-X surface is always on the outside, so start there.
+		visited = set()
+		open = {(voxel, surface)}
+		while open:
+			voxel, surface = open.pop()
+			assert (voxel[0] + surface[0], voxel[1] + surface[1], voxel[2] + surface[2]) not in self.voxels
+			visited.add((voxel, surface))
+			surface_dim = 0  # Find the dimension at which the current surface is.
+			while surface[surface_dim] == 0:
+				surface_dim += 1
+			for direction in [(0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)]:  # Check all four adjacent surfaces.
+				dx, dy, dz = direction[surface_dim], direction[(surface_dim + 2) % 3], direction[(surface_dim + 1) % 3]  # Rotate according to surface_dim.
+				diagonal = voxel[0] + dx + surface[0], voxel[1] + dy + surface[1], voxel[2] + dz + surface[2]  # First check for the diagonal voxel that looks perpendicularly over the current surface.
+				if diagonal in self.voxels:  # The neighbouring voxel there is occupied.
+					if (diagonal, (-dx, -dy, -dz)) not in visited:
+						open.add((diagonal, (-dx, -dy, -dz)))  # Opposite to the direction we were initially checking.
+				else:
+					neighbour = voxel[0] + dx, voxel[1] + dy, voxel[2] + dz  # Then check the straight neighbour that could cover the surface in this direction.
+					if neighbour in self.voxels:
+						if (neighbour, (surface[0], surface[1], surface[2])) not in visited:
+							open.add((neighbour, (surface[0], surface[1], surface[2])))  # Same as the direction we were initially checking.
+					else:
+						if (voxel, (dx, dy, dz)) not in visited:
+							open.add((voxel, (dx, dy, dz)))
+
+		return len(visited)
