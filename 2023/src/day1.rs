afba5cd89d95837a14313653fb21be27ba586976
Ghostkeeper <rubend@tutanota.com>
2023-12-01 12:39:16 +0100

    Implement part 1 of day 1

--- 2023/src/day1.rs
+++ 2023/src/day1.rs
@@ -1,3 +1,13 @@
+use regex::Regex;
+
 pub fn part1(input: String) {
-	
+	let mut sum = 0;
+	let re = Regex::new(r"[^0-9]").unwrap();
+	for line in input.split("\n") {
+		let filtered = re.replace_all(line, "");
+		let first = filtered.chars().next().unwrap() as i32 - 48;
+		let last = filtered.chars().last().unwrap() as i32 - 48;
+		sum += first * 10 + last;
+	}
+	println!("{}", sum);
 }
