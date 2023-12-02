4d4c8da2693606941f6d00de45b1677d0e1b5eae
Ghostkeeper <rubend@tutanota.com>
2023-12-02 09:34:38 +0100

    Implement part 2 of day 1

--- 2023/src/day1.rs
+++ 2023/src/day1.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use regex::Regex;
 
 pub fn part1(input: String) {
@@ -8,6 +9,80 @@ pub fn part1(input: String) {
 		let first = filtered.chars().next().unwrap() as i32 - 48;
 		let last = filtered.chars().last().unwrap() as i32 - 48;
 		sum += first * 10 + last;
+	}
+	println!("{}", sum);
+}
+
+pub fn part2(input: String) {
+	let replacement = HashMap::from([
+		// ("eleven", "11"),
+		// ("twelve", "12"),
+		// ("thirteen", "13"),
+		// ("fourteen", "14"),
+		// ("fifteen", "15"),
+		// ("sixteen", "16"),
+		// ("seventeen", "17"),
+		// ("eighteen", "18"),
+		// ("nineteen", "19"),
+		// ("ten", "10"),
+		// ("twenty", "20"),
+		// ("thirty", "30"),
+		// ("fourty", "40"),
+		// ("fifty", "50"),
+		// ("sixty", "60"),
+		// ("seventy", "70"),
+		// ("eighty", "80"),
+		// ("ninety", "90"),
+		// ("hundred", "100"),
+		("one", 1),
+		("1", 1),
+		("two", 2),
+		("2", 2),
+		("three", 3),
+		("3", 3),
+		("four", 4),
+		("4", 4),
+		("five", 5),
+		("5", 5),
+		("six", 6),
+		("6", 6),
+		("seven", 7),
+		("7", 7),
+		("eight", 8),
+		("8", 8),
+		("nine", 9),
+		("9", 9),
+		// ("zero", "0"),
+	]);
+	let mut sum = 0;
+	let re = Regex::new(r"[^0-9]").unwrap();
+	for line in input.split("\n") {
+		let mut first = 0;
+		let mut last = 0;
+		let mut i = 0 as i32;
+		while i < line.len() as i32 {
+			for (key, val) in replacement.iter() {
+				if line[i as usize..].starts_with(key) {
+					first = *val;
+					i = line.len() as i32;
+					break;
+				}
+			}
+			i += 1;
+		}
+		i = line.len() as i32 - 1;
+		while i >= 0 {
+			for (key, val) in replacement.iter() {
+				if line[i as usize..].starts_with(key) {
+					last = *val;
+					i = -1;
+					break;
+				}
+			}
+			i -= 1;
+		}
+		sum += first * 10 + last;
+		println!("{}  {}", line, first * 10 + last);
 	}
 	println!("{}", sum);
 }
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -4,5 +4,5 @@ mod day1;
 
 fn main() {
 	let contents = fs::read_to_string("inputs/day1_1.txt").expect("File should exist.");
-	day1::part1(contents);
+	day1::part2(contents);
 }
