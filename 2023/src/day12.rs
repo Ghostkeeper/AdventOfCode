b3ec7c31daaf8a80e0a0cf9053b3d75466e12d1e
Ghostkeeper <rubend@tutanota.com>
2023-12-12 16:59:29 +0100

    Speed-up by using String instead of Vec<char>
    Not sure if it's really a speed-up, but it's nicer in many ways.

--- 2023/src/day12.rs
+++ 2023/src/day12.rs
@@ -1,27 +1,27 @@
 use itertools::Itertools;
 
-fn parse(input: String) -> (Vec<Vec<char>>, Vec<Vec<i32>>) {
+fn parse(input: String) -> (Vec<String>, Vec<Vec<i32>>) {
 	let mut fields = vec!();
 	let mut sequences = vec!();
 
 	for line in input.split("\n") {
 		let mut parts = line.split_whitespace();
-		fields.push(parts.next().unwrap().chars().collect_vec());
+		fields.push(parts.next().unwrap().to_string());
 		sequences.push(parts.next().unwrap().split(",").map(|n| n.parse::<i32>().unwrap()).collect_vec());
 	}
 	return (fields, sequences);
 }
 
-fn is_valid(field: &Vec<char>, sequence: &Vec<i32>) -> bool {
+fn is_valid(field: &[u8], sequence: &Vec<i32>) -> bool {
 	let mut pos = 0usize;
 	for streak in sequence {
 		//Find when the streak starts.
-		while pos < field.len() && field[pos] == '.' {
+		while pos < field.len() && field[pos] == '.' as u8 {
 			pos += 1;
 		}
 		//Find this streak's length.
 		let mut length = 0;
-		while pos < field.len() && field[pos] == '#' {
+		while pos < field.len() && field[pos] == '#' as u8 {
 			length += 1;
 			pos += 1;
 		}
@@ -31,7 +31,7 @@ fn is_valid(field: &Vec<char>, sequence: &Vec<i32>) -> bool {
 	}
 	//Find if there are any more streaks after the last.
 	while pos < field.len() {
-		if field[pos] == '#' {
+		if field[pos] == '#' as u8 {
 			return false; //Too many streaks.
 		}
 		pos += 1;
@@ -39,27 +39,29 @@ fn is_valid(field: &Vec<char>, sequence: &Vec<i32>) -> bool {
 	return true;
 }
 
-fn num_valid(field: &Vec<char>, sequence: &Vec<i32>) -> usize {
-	let num_unknown = field.iter().filter(|c| **c == '?').count();
-	let num_damaged_known = field.iter().filter(|c| **c == '#').count();
+fn num_valid(field: &[u8], sequence: &Vec<i32>) -> usize {
+	let num_unknown = field.iter().filter(|c| **c == '?' as u8).count();
+	let num_damaged_known = field.iter().filter(|c| **c == '#' as u8).count();
 	let num_damaged = sequence.iter().sum::<i32>() as u32;
 	let num_damaged_unknown: u32 = num_damaged - (num_damaged_known as u32);
 	let mut valid = 0usize;
 	let mut bitfield = 2usize.pow(num_damaged_unknown) - 1;
 	while bitfield <= 2usize.pow(num_damaged_unknown) - 1 << (num_unknown - num_damaged_unknown as usize) {
-		let mut filled_in = field.clone();
+		let mut filled_in = String::new();
 		let mut bit = 0;
-		for (pos, char) in field.iter().enumerate() {
-			if *char == '?' {
+		for c in field {
+			if *c == '?' as u8 {
 				if bitfield & (1 << bit) > 0 {
-					filled_in[pos] = '#';
+					filled_in.push('#');
 				} else {
-					filled_in[pos] = '.';
+					filled_in.push('.');
 				}
 				bit += 1;
+			} else {
+				filled_in.push(*c as char);
 			}
 		}
-		if is_valid(&filled_in, sequence) {
+		if is_valid(&filled_in.as_bytes(), sequence) {
 			valid += 1;
 		}
 
@@ -74,7 +76,7 @@ pub fn part1(input: String) {
 	let (fields, sequences) = parse(input);
 	let mut sum_arrangements = 0;
 	for i in 0..fields.len() {
-		let num = num_valid(&fields[i], &sequences[i]);
+		let num = num_valid(fields[i].as_bytes(), &sequences[i]);
 		sum_arrangements += num;
 	}
 	println!("{}", sum_arrangements);
@@ -84,13 +86,13 @@ pub fn part2(input: String) {
 	let (fields, sequences) = parse(input);
 	let mut sum_arrangements = 0;
 	for i in 0..fields.len() {
-		let mut multiplied_field: Vec<char> = vec!();
+		let mut multiplied_field = String::new();
 		let mut multiplied_sequence: Vec<i32> = vec!();
-		for n in 0..5 {
-			multiplied_field.append(&mut fields[i].clone());
+		for _ in 0..5 {
+			multiplied_field += &fields[i];
 			multiplied_sequence.append(&mut sequences[i].clone());
 		}
-		let num = num_valid(&multiplied_field, &multiplied_sequence);
+		let num = num_valid(multiplied_field.as_bytes(), &multiplied_sequence);
 		sum_arrangements += num;
 	}
 	println!("{}", sum_arrangements);
