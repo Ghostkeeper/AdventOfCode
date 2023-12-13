05655bf3d886f7a964f5bf60cb216502797e9a29
Ghostkeeper <rubend@tutanota.com>
2023-12-13 22:08:50 +0100

    Implement part 2 of day 13

--- 2023/src/day13.rs
+++ 2023/src/day13.rs
@@ -13,21 +13,22 @@ fn parse(input: String) -> Vec<Vec<Vec<bool>>> {
     return result;
 }
 
-fn find_reflection(field: &Vec<Vec<bool>>) -> usize {
+fn find_reflection(field: &Vec<Vec<bool>>, required_num_errors: i32) -> usize {
     for y in 1..field.len() {
-        if test_reflection_horizontal(&field, y) {
+        if test_reflection_horizontal(&field, y) == required_num_errors {
             return y * 100;
         }
     }
     for x in 1..field[0].len() {
-        if test_reflection_vertical(&field, x) {
+        if test_reflection_vertical(&field, x) == required_num_errors {
             return x;
         }
     }
-    panic!("No reflection found!");
+    return 0; //Meaning: No reflection found.
 }
 
-fn test_reflection_horizontal(field: &Vec<Vec<bool>>, reflect_y: usize) -> bool {
+fn test_reflection_horizontal(field: &Vec<Vec<bool>>, reflect_y: usize) -> i32 {
+    let mut num_errors = 0;
     for x in 0..field[0].len() {
         for dy in 0..(field.len() / 2) {
             let yplus = reflect_y + dy;
@@ -36,14 +37,15 @@ fn test_reflection_horizontal(field: &Vec<Vec<bool>>, reflect_y: usize) -> bool 
                 break; //Don't go out of bounds.
             }
             if field[yplus][x] != field[ymin as usize][x] {
-                return false;
+                num_errors += 1;
             }
         }
     }
-    return true;
+    return num_errors;
 }
 
-fn test_reflection_vertical(field: &Vec<Vec<bool>>, reflect_x: usize) -> bool {
+fn test_reflection_vertical(field: &Vec<Vec<bool>>, reflect_x: usize) -> i32 {
+    let mut num_errors = 0;
     for y in 0..field.len() {
         for dx in 0..(field[0].len() / 2) {
             let xplus = reflect_x + dx;
@@ -52,18 +54,27 @@ fn test_reflection_vertical(field: &Vec<Vec<bool>>, reflect_x: usize) -> bool {
                 break; //Don't go out of bounds.
             }
             if field[y][xplus] != field[y][xmin as usize] {
-                return false;
+                num_errors += 1;
             }
         }
     }
-    return true;
+    return num_errors;
 }
 
 pub fn part1(input: String) -> usize {
     let fields = parse(input);
     let mut sum = 0;
     for field in fields {
-        sum += find_reflection(&field);
+        sum += find_reflection(&field, 0);
+    }
+    return sum;
+}
+
+pub fn part2(input: String) -> usize {
+    let mut fields = parse(input);
+    let mut sum = 0;
+    for mut field in fields {
+        sum += find_reflection(&mut field, 1);
     }
     return sum;
 }
