f817511e70dc17b767d7633c519010dd1c054e13
Ghostkeeper <rubend@tutanota.com>
2023-12-20 23:20:15 +0100

    Implement part 1 of day 20
    Ugh. I hate Rust's borrow checker now. Clone all the things.

--- 2023/src/day20.rs
+++ 2023/src/day20.rs
@@ -1,10 +1,12 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use itertools::Itertools;
 
 #[derive(Clone, Debug)]
 struct Node {
 	module: char,
 	output: Vec<String>,
+	flipflop_on: bool,
+	conjunction_store: HashMap<String, bool>,
 }
 
 fn parse(input: String) -> HashMap<String, Node> {
@@ -22,6 +24,8 @@ fn parse(input: String) -> HashMap<String, Node> {
 			nodes.insert(name, Node {
 				module: 'b',
 				output,
+				flipflop_on: false,
+				conjunction_store: HashMap::new(),
 			});
 		} else {
 			let module = name.chars().next().unwrap();
@@ -29,15 +33,86 @@ fn parse(input: String) -> HashMap<String, Node> {
 			nodes.insert(stripped_name, Node {
 				module,
 				output,
+				flipflop_on: false,
+				conjunction_store: HashMap::new(),
 			});
+		}
+	}
+	nodes.insert("output".to_string(), Node {
+		module: 'b',
+		output: vec!(),
+		flipflop_on: false,
+		conjunction_store: HashMap::new(),
+	});
+
+	//Fill the conjunction stores.
+	let names = nodes.keys().map(|n| n.clone()).collect_vec();
+	for name in names {
+		for target in nodes.get(&name).unwrap().output.clone() {
+			match nodes.get_mut(&target) {
+				Some(somenode) => { somenode.conjunction_store.insert(name.clone(), false); },
+				None => println!("Can't find node {}", target),
+			}
 		}
 	}
 
 	return nodes;
 }
 
+struct Output {
+	lows: u32,
+	highs: u32,
+}
+
+fn do_pulse(nodes: &mut HashMap<String, Node>, queue: &mut VecDeque<(String, bool, String)>, node_name: &String, is_high: bool, source: String, output: &mut Output) {
+	if is_high {
+		output.highs += 1;
+	} else {
+		output.lows += 1;
+	}
+	if !nodes.contains_key(node_name) {
+		return; //Don't do any pulse I guess.
+	}
+	let module = nodes.get(node_name).unwrap().module;
+	match module {
+		'b' => { //Broadcaster.
+			for target in nodes.get(node_name).unwrap().output.clone().iter() {
+				queue.push_back((node_name.clone(), is_high, target.clone()));
+			}
+		},
+		'%' => {
+			if !is_high {
+				let original_flip = nodes.get(node_name).unwrap().flipflop_on;
+				nodes.get_mut(node_name).unwrap().flipflop_on = !original_flip;
+				for target in nodes.get(node_name).unwrap().output.clone().iter() {
+					queue.push_back((node_name.clone(), !original_flip, target.clone()));
+				}
+			}
+		},
+		'&' => {
+			nodes.get_mut(node_name).unwrap().conjunction_store.insert(source.clone(), is_high);
+			let send_low = nodes.get(node_name).unwrap().conjunction_store.values().all(|b| *b);
+			for target in nodes.get(node_name).unwrap().output.clone().iter() {
+				queue.push_back((node_name.clone(), !send_low, target.clone()));
+			}
+		}
+		_ => panic!("Unknown node module type."),
+	}
+}
+
 pub fn part1(input: String) -> u32 {
-	let network = parse(input);
-	println!("{:?}", network.values());
-	return 0;
+	let mut network = parse(input);
+	let mut output = Output {
+		lows: 0,
+		highs: 0,
+	};
+	let mut queue: VecDeque<(String, bool, String)> = VecDeque::new();
+	for _ in 0..1000 {
+		queue.push_back(("button".to_string(), false, "broadcaster".to_string()));
+		while !queue.is_empty() {
+			let (source, is_high, target) = queue.pop_front().unwrap();
+			do_pulse(&mut network, &mut queue, &target, is_high, source, &mut output);
+		}
+	}
+	return output.highs * output.lows;
 }
