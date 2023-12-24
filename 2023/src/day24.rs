ad76ccbc890f4cc8c5fd0bcd0a5086c29abca9ef
Ghostkeeper <rubend@tutanota.com>
2023-12-24 13:23:54 +0100

    Try day 24 with my own intersection algorithm
    I wrote this one years ago, and it's super efficient and integer-accurate. I hope integer-accurate will be enough.

--- 2023/src/day24.rs
+++ 2023/src/day24.rs
@@ -1,21 +1,42 @@
-use geo::Line;
-use intersect2d::{intersect, Intersection};
 use itertools::Itertools;
 use regex::Regex;
 
-fn parse(input: String) -> Vec<(f64, f64, f64, f64, f64, f64)> {
+fn parse(input: String) -> Vec<(i128, i128, i128, i128, i128, i128)> {
 	let re = Regex::new(r"(?<x>\d+), (?<y>\d+), (?<z>\d+) @  ?(?<dx>-?\d+),  ?(?<dy>-?\d+),  ?(?<dz>-?\d+)").unwrap();
 	let hails = re.captures_iter(input.as_str()).map(|caps| {
-		let x = caps.name("x").unwrap().as_str().parse::<f64>().unwrap();
-		let y = caps.name("y").unwrap().as_str().parse::<f64>().unwrap();
-		let z = caps.name("z").unwrap().as_str().parse::<f64>().unwrap();
-		let dx = caps.name("dx").unwrap().as_str().parse::<f64>().unwrap();
-		let dy = caps.name("dy").unwrap().as_str().parse::<f64>().unwrap();
-		let dz = caps.name("dz").unwrap().as_str().parse::<f64>().unwrap();
+		let x = caps.name("x").unwrap().as_str().parse::<i128>().unwrap();
+		let y = caps.name("y").unwrap().as_str().parse::<i128>().unwrap();
+		let z = caps.name("z").unwrap().as_str().parse::<i128>().unwrap();
+		let dx = caps.name("dx").unwrap().as_str().parse::<i128>().unwrap();
+		let dy = caps.name("dy").unwrap().as_str().parse::<i128>().unwrap();
+		let dz = caps.name("dz").unwrap().as_str().parse::<i128>().unwrap();
 		(x, y, z, dx, dy, dz)
 	}).collect_vec();
 
 	return hails;
+}
+
+fn cross_product(a: (i128, i128), b: (i128, i128)) -> i128 {
+	a.0 * b.1 - a.1 * b.0
+}
+
+fn round_divide(numerator: i128, denominator: i128) -> i128 {
+	if (numerator < 0) ^ (denominator < 0) {
+		(numerator - denominator / 2) / denominator
+	} else {
+		(numerator + denominator / 2) / denominator
+	}
+}
+
+fn intersect_line_2d(line1: (i128, i128, i128, i128, i128, i128), line2: (i128, i128, i128, i128, i128, i128)) -> (i128, i128) {
+	let divisor = cross_product((line1.3, line1.4), (line2.3, line2.4));
+	if divisor == 0 { //They don't intersect.
+		return (i128::MIN, i128::MIN);
+	}
+	let starts_delta = (line1.0 - line2.0, line1.1 - line2.1);
+	let line1_parametric = cross_product((line2.3, line2.4), starts_delta);
+	let line2_parametric = cross_product((line1.3, line1.4), starts_delta);
+	return (line1.0 + round_divide(line1_parametric * line1.3, divisor), line1.1 + round_divide(line1_parametric * line1.4, divisor));
 }
 
 pub fn part1(input: String) -> usize {
@@ -25,22 +46,11 @@ pub fn part1(input: String) -> usize {
 	let mut count = 0;
 	for hail_id1 in 0..hails.len() {
 		for hail_id2 in 0..hail_id1 {
-			let line1 = Line::<f64> {
-				start: (hails[hail_id1].0, hails[hail_id1].1).into(),
-				end: (hails[hail_id1].0 + hails[hail_id1].3, hails[hail_id1].1 + hails[hail_id1].4).into(),
-			};
-			let line2 = Line::<f64> {
-				start: (hails[hail_id2].0, hails[hail_id2].1).into(),
-				end: (hails[hail_id2].0 + hails[hail_id2].3, hails[hail_id2].1 + hails[hail_id2].4).into(),
-			};
-			let rv = intersect(&line1, &line2);
-			match rv {
-				Some(Intersection::Intersection(a)) => println!("Intersection {:?} and {:?}: {:?}", line1, line2, a),
-				_ => println!("These lines don't intersect."),
+			let intersection = intersect_line_2d(hails[hail_id1], hails[hail_id2]);
+			println!("Lines {:?} x {:?} intersect at {:?}", hails[hail_id1], hails[hail_id2], intersection);
+			if intersection.0 >= 7 && intersection.0 <= 27 && intersection.1 >= 7 && intersection.1 <= 27 {
+				count += 1;
 			}
-			// if intersection.0 >= 7 && intersection.0 <= 27 && intersection.1 >= 7 && intersection.1 <= 27 {
-			// 	count += 1;
-			// }
 		}
 	}
 
