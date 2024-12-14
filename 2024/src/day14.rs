0731e0d3ac70bec3969996c5c1ffeb786dcbd6c2
Ghostkeeper <rubend@tutanota.com>
2024-12-14 09:08:42 +0100

    Adjustment to make part 2 actually give the answer
    I found it manually. But we can do the same thing automatically.

--- 2024/src/day14.rs
+++ 2024/src/day14.rs
@@ -1,3 +1,5 @@
+use std::fs;
+use std::os::linux::fs::MetadataExt;
 use image::*;
 use itertools::Itertools;
 use regex::Regex;
@@ -95,7 +97,7 @@ pub fn part1(input: String) -> u64 {
 	return score(robots);
 }
 
-pub fn part2(input: String) -> u64 {
+pub fn part2(input: String) -> i32 {
 	let mut robots = parse(input);
 
 	let mut i = 0;
@@ -119,5 +121,16 @@ pub fn part2(input: String) -> u64 {
 		save_buffer(filename, &buffer, WIDTH as u32, HEIGHT as u32, ExtendedColorType::L8).unwrap();
 	}
 
-	return i;
+	let mut best = 0;
+	let mut lowest_size = 999999999;
+	for i in 1..(WIDTH * HEIGHT) {
+		let filename = format!("step{}.png", i);
+		let fsize = fs::metadata(filename).unwrap().st_size();
+		if fsize < lowest_size {
+			lowest_size = fsize;
+			best = i;
+		}
+	}
+
+	return best;
 }
