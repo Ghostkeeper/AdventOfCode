97096638a60a5dba19342e4c2940b07fa7906112
Ghostkeeper <rubend@tutanota.com>
2024-12-15 10:27:43 +0100

    Implement part 2 of day 15

--- 2024/src/day15.rs
+++ 2024/src/day15.rs
@@ -86,3 +86,95 @@ pub fn part1(input: String) -> usize {
 	}
 	return sum_gps;
 }
+
+fn can_push(grid: &Vec<Vec<char>>, position: (i32, i32), step: (i32, i32)) -> bool {
+	let push_to = ((position.0 + step.0) as usize, (position.1 + step.1) as usize);
+	if grid[push_to.1][push_to.0] == '.' {
+		return true;
+	}
+	if grid[push_to.1][push_to.0] == '#' {
+		return false;
+	}
+	if grid[push_to.1][push_to.0] == '[' {
+		if step.1 != 0 { //Vertical movement.
+			return can_push(&grid, (push_to.0 as i32, push_to.1 as i32), step)
+				&& can_push(&grid, (push_to.0 as i32 + 1, push_to.1 as i32), step);
+		} else {
+			return can_push(&grid, (position.0 + step.0 * 2, position.1), step);
+		}
+	}
+	if grid[push_to.1][push_to.0] == ']' {
+		if step.1 != 0 { //Vertical movement.
+			return can_push(&grid, (push_to.0 as i32, push_to.1 as i32), step)
+				&& can_push(&grid, (push_to.0 as i32 - 1, push_to.1 as i32), step);
+		} else {
+			return can_push(&grid, (position.0 + step.0 * 2, position.1), step);
+		}
+	}
+	panic!();
+}
+
+fn push(grid: &mut Vec<Vec<char>>, position: &mut (i32, i32), step: (i32, i32)) {
+	let push_to = ((position.0 + step.0) as usize, (position.1 + step.1) as usize);
+	if grid[push_to.1][push_to.0] == '.' {
+		grid[push_to.1][push_to.0] = grid[position.1 as usize][position.0 as usize];
+		grid[position.1 as usize][position.0 as usize] = '.';
+		position.0 += step.0;
+		position.1 += step.1;
+	} else if grid[push_to.1][push_to.0] == '#' {
+		panic!("I thought we could push to this place!");
+	} else if grid[push_to.1][push_to.0] == '[' {
+		if step.1 != 0 { //Vertical movement.
+			push(grid, &mut (position.0 + step.0, position.1 + step.1), step);
+			push(grid, &mut (position.0 + step.0 + 1, position.1 + step.1), step);
+			push(grid, position, step); //Try again.
+		} else {
+			push(grid, &mut (position.0 + step.0, position.1), step);
+			push(grid, position, step); //Try again.
+		}
+	} else if grid[push_to.1][push_to.0] == ']' {
+		if step.1 != 0 { //Vertical movement.
+			push(grid, &mut (position.0 + step.0, position.1 + step.1), step);
+			push(grid, &mut (position.0 + step.0 - 1, position.1 + step.1), step);
+			push(grid, position, step); //Try again.
+		} else {
+			push(grid, &mut (position.0 + step.0, position.1), step);
+			push(grid, position, step); //Try again.
+		}
+	}
+}
+
+pub fn part2(input: String) -> usize {
+	let (mut grid, moves, mut robot) = parse(input);
+	//Double up the grid.
+	robot.0 *= 2;
+	for y in 0..grid.len() {
+		let mut new_row = vec!();
+		for cell in &grid[y] {
+			if *cell == 'O' {
+				new_row.push('[');
+				new_row.push(']');
+			} else {
+				new_row.push(*cell);
+				new_row.push(*cell);
+			}
+		}
+		grid[y] = new_row;
+	}
+
+	for step in moves {
+		if can_push(&grid, robot.clone(), step) {
+			push(&mut grid, &mut robot, step);
+		}
+	}
+
+	let mut sum_gps = 0;
+	for y in 0..grid.len() {
+		for x in 0..grid[0].len() {
+			if grid[y][x] == '[' {
+				sum_gps += y * 100 + x;
+			}
+		}
+	}
+	return sum_gps;
+}
