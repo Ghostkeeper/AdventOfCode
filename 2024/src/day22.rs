7b8222e6f1c881d821bb5a1bfbe5a38c4b7ffd68
Ghostkeeper <rubend@tutanota.com>
2024-12-22 09:37:38 +0100

    Implement part 2 of day 22
    Still running, but I want to commit so that I may multithread this one.

--- 2024/src/day22.rs
+++ 2024/src/day22.rs
@@ -1,10 +1,10 @@
 use itertools::Itertools;
 
-fn parse(input: String) -> Vec<u64> {
-	input.split("\n").map(|line| line.parse::<u64>().unwrap()).collect_vec()
+fn parse(input: String) -> Vec<i32> {
+	input.split("\n").map(|line| line.parse::<i32>().unwrap()).collect_vec()
 }
 
-fn pseudorandom(mut n: u64) -> u64 {
+fn pseudorandom(mut n: i32) -> i32 {
 	n = ((n << 6) ^ n) & 0xFFFFFF;
 	n = ((n >> 5) ^ n) & 0xFFFFFF;
 	n = ((n << 11) ^ n) & 0xFFFFFF;
@@ -13,12 +13,52 @@ fn pseudorandom(mut n: u64) -> u64 {
 
 pub fn part1(input: String) -> u64 {
 	let secrets = parse(input);
-	let mut sum = 0;
+	let mut sum = 0u64;
 	for mut secret in secrets {
 		for _ in 0..2000 {
 			secret = pseudorandom(secret);
 		}
-		sum += secret;
+		sum += secret as u64;
 	}
 	sum
+}
+
+pub fn part2(input: String) -> i32 {
+	let secrets = parse(input);
+
+	let mut max_profit = i32::MIN;
+	for d1 in -9..10 {
+		for d2 in -9..10 {
+			for d3 in -9..10 {
+				for d4 in -9..10 {
+					if d1 + d2 + d3 + d4 < -9 {
+						continue;
+					}
+					let mut profit = 0;
+					for mut secret in secrets.clone() {
+						let (mut diff1, mut diff2, mut diff3, mut diff4) = (100, 100, 100, 100);
+						for _ in 0..2000 {
+							let previous = secret;
+							secret = pseudorandom(secret);
+							diff1 = diff2;
+							diff2 = diff3;
+							diff3 = diff4;
+							diff4 = (secret % 10) - (previous % 10);
+							if d1 == diff1 && d2 == diff2 && d3 == diff3 && d4 == diff4 {
+								//Sell!
+								profit += secret % 10;
+								break;
+							}
+						}
+					}
+					if profit > max_profit {
+						max_profit = profit;
+						println!("Found max profit so far ({}) using sequence {} {} {} {}", profit, d1, d2, d3, d4);
+					}
+				}
+			}
+		}
+	}
+
+	max_profit
 }
--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -33,7 +33,7 @@ static GLOBAL: Jemalloc = Jemalloc;
 fn main() {
 	let contents = fs::read_to_string("inputs/day22_1.txt").expect("File should exist.");
 	let start = Instant::now();
-	let answer = day22::part1(contents);
+	let answer = day22::part2(contents);
 	let elapsed = start.elapsed();
 	println!("ANSWER: {}", answer);
 	println!("Elapsed time: {:.2?}", elapsed);
