988ab8bb4ea8652d18cda9aabbef43bf84994b74
Ghostkeeper <rubend@tutanota.com>
2025-12-03 18:51:39 +0100

    Implement part 2 of day 3

--- 2025/src/day3.rs
+++ 2025/src/day3.rs
@@ -19,3 +19,21 @@ pub fn part1(input: String) -> u32 {
 	}
 	result
 }
+
+pub fn part2(input: String) -> u64 {
+	let mut result = 0;
+	let banks = parse(input);
+	for bank in banks {
+		let mut current_bank = &bank[..];
+		let mut joltage = 0;
+		for i in 0..12 {
+			let bank_minus_end = &current_bank[0..current_bank.len() - (11 - i)];
+			let digit = bank_minus_end.iter().max().unwrap();
+			let index_of_digit = bank_minus_end.iter().position(|x| x == digit).unwrap();
+			current_bank = &current_bank[(index_of_digit + 1)..];
+			joltage += *digit as u64 * 10u64.pow(11 - i as u32);
+		}
+		result += joltage;
+	}
+	result
+}
