780f6456d9affac7540109ae84f39e42849eb04e
Ghostkeeper <rubend@tutanota.com>
2025-12-09 00:55:27 +0100

    Implement part 2 of day 8
    Actually had to optimise a bit to keep to our challenge this year.

--- 2025/src/day8.rs
+++ 2025/src/day8.rs
@@ -14,28 +14,11 @@ fn distance(v1: (i64, i64, i64), v2: (i64, i64, i64)) -> i64 {
     (v1.0 - v2.0) * (v1.0 - v2.0) + (v1.1 - v2.1) * (v1.1 - v2.1) + (v1.2 - v2.2) * (v1.2 - v2.2)
 }
 
-fn connect_closest(vertices: &Vec<(i64, i64, i64)>, connected: &mut Vec<Vec<usize>>) {
-    let mut smallest_distance = i64::MAX;
-    let mut smallest_i = 0;
-    let mut smallest_j = 0;
-    for i in 0..vertices.len() {
-        for j in 0..i {
-            if connected[i].contains(&j) {
-                continue;
-            }
-            let dist = distance(vertices[i], vertices[j]);
-            if dist < smallest_distance {
-                smallest_distance = dist;
-                smallest_i = i;
-                smallest_j = j;
-            }
-        }
-    }
-    if smallest_i == 0 && smallest_j == 0 {
-        panic!("Couldn't find anything to connect.")
-    }
-    connected[smallest_i].push(smallest_j);
-    connected[smallest_j].push(smallest_i);
+fn connect_closest(vertices: &Vec<(i64, i64, i64)>, connected: &mut Vec<Vec<usize>>, dists: &mut Vec<(i64, usize, usize)>) -> i64 {
+    let (_dist, i, j) = dists.pop().expect("Ran out of vertices to connect.");
+    connected[i].push(j);
+    connected[j].push(i);
+    vertices[i].0 * vertices[j].0
 }
 
 fn circuit_sizes(connected: &Vec<Vec<usize>>) -> Vec<usize> {
@@ -71,16 +54,71 @@ fn circuit_sizes(connected: &Vec<Vec<usize>>) -> Vec<usize> {
     circuits
 }
 
+fn is_connected(connected: &Vec<Vec<usize>>) -> bool {
+    let mut found = vec!();
+    for _ in 0..connected.len() {
+        found.push(false);
+    }
+
+    let mut todo = vec!();
+    todo.push(0);
+    while !todo.is_empty() {
+        let neighbour = todo.pop().unwrap();
+        for j in 0..connected[neighbour].len() {
+            let adjacent = connected[neighbour][j];
+            if found[adjacent] {
+                continue;
+            }
+            found[adjacent] = true;
+            todo.push(adjacent);
+        }
+    }
+    for is_found in found {
+        if !is_found {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn distances(vertices: &Vec<(i64, i64, i64)>) -> Vec<(i64, usize, usize)> {
+    let mut result = vec!();
+    for i in 0..vertices.len() {
+        for j in 0..i {
+            result.push((distance(vertices[i], vertices[j]), i, j));
+        }
+    }
+    result.sort_unstable_by_key(|d| d.0);
+    result.reverse();
+    result
+}
+
 pub fn part1(input: String) -> usize {
     let vertices = parse(input);
+    let mut dists = distances(&vertices);
     let mut connected = vec!();
     for _ in 0..vertices.len() {
         connected.push(vec!());
     }
     for _ in 0..1000 {
-        connect_closest(&vertices, &mut connected);
+        connect_closest(&vertices, &mut connected, &mut dists);
     }
     let mut sizes = circuit_sizes(&connected);
     sizes.sort_unstable();
     sizes[sizes.len() - 1] * sizes[sizes.len() - 2] * sizes[sizes.len() - 3]
+}
+
+pub fn part2(input: String) -> i64 {
+    let vertices = parse(input);
+    let mut dists = distances(&vertices);
+    let mut connected = vec!();
+    for _ in 0..vertices.len() {
+        connected.push(vec!());
+    }
+
+    let mut answer = 0;
+    while !is_connected(&connected) {
+        answer = connect_closest(&vertices, &mut connected, &mut dists);
+    }
+    answer
 }
