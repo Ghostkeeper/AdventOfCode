280f5786c0675f83ad2f330c29b188a5fc810981
Ghostkeeper <rubend@tutanota.com>
2025-12-09 23:55:28 +0100

    Implement part 2 of day 9

--- 2025/src/day9.rs
+++ 2025/src/day9.rs
@@ -24,3 +24,31 @@ pub fn part1(input: String) -> i64 {
 
 	biggest_area
 }
+
+pub fn part2(input: String) -> i64 {
+	let vertices = parse(input);
+	let mut biggest_area = 0;
+
+	for i in 0..vertices.len() {
+		'rectfind: for j in 0..i {
+			let v1 = vertices[i];
+			let v2 = vertices[j];
+			let minx = v1.0.min(v2.0);
+			let miny = v1.1.min(v2.1);
+			let maxx = v1.0.max(v2.0);
+			let maxy = v1.1.max(v2.1);
+			for k in 0..vertices.len() {
+				let edge1 = vertices[k];
+				let edge2 = vertices[(k + 1) % vertices.len()];
+				if !(edge1.0 <= minx && edge2.0 <= minx) && !(edge1.1 <= miny && edge2.1 <= miny) && !(edge1.0 >= maxx && edge2.0 >= maxx) && !(edge1.1 >= maxy && edge2.1 >= maxy) {
+					//Intersects with the rectangle. Not allowed then!
+					continue 'rectfind;
+				}
+			}
+			//Nothing intersects. So this is allowed!
+			biggest_area = biggest_area.max(area(&v1, &v2));
+		}
+	}
+
+	biggest_area
+}
