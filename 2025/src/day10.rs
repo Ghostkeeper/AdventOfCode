db2f3895a4c25e8e18eb607cd67f04286d191fd7
Ghostkeeper <rubend@tutanota.com>
2025-12-12 17:13:50 +0100

    Properly implement part 2 of day 10
    Took too long though. More than 1 second!

--- 2025/src/day10.rs
+++ 2025/src/day10.rs
@@ -87,44 +87,70 @@ pub fn part1(input: String) -> i64 {
     total
 }
 
-fn dfs2(state: &mut Vec<u32>, machine: &Machine, path_len: usize, cache: &mut HashMap<(Vec<u32>, usize), i64>) -> i64 {
-    if *state == machine.joltages {
+fn dfs2(target_joltages: &Vec<u32>, buttons_allowed: u32, buttons: &Vec<Vec<usize>>) -> u32 {
+    if target_joltages.iter().all(|j| *j == 0) {
         return 0;
     }
-    for i in 0..state.len() {
-        if state[i] > machine.joltages[i] {
-            return -1; //Joltage too high!
+
+    let (lowfreq_button, &lowfreq) = target_joltages.iter().enumerate().filter(|&(_, &joltage)| joltage > 0).min_by_key(|&(i, &joltage)| {
+        (buttons.iter().enumerate().filter(|&(j, b)| {
+            buttons_allowed & (1 << j) != 0 && b.contains(&i)
+        }).count(), -(joltage as i32))
+    }).unwrap();
+
+    let matching_buttons = buttons.iter().enumerate().filter(|&(i, button)| buttons_allowed & (1 << i) != 0 && button.contains(&lowfreq_button)).collect::<Vec<_>>();
+    let mut result = u32::MAX;
+
+    if !matching_buttons.is_empty() {
+        let mut new_buttons_allowed = buttons_allowed;
+        for (i, _) in &matching_buttons {
+            new_buttons_allowed &= !(1 << i);
+        }
+
+        //Try all allowed buttons.
+        let mut new_joltage = target_joltages.clone();
+        let mut counts = vec![0; matching_buttons.len()];
+        counts[matching_buttons.len() - 1] = lowfreq;
+        loop {
+            let mut has_button = true;
+            new_joltage.copy_from_slice(target_joltages);
+            'buttons: for (button_index, &count) in counts.iter().enumerate() {
+                if count == 0 {
+                    continue;
+                }
+                for &joltage_index in matching_buttons[button_index].1 {
+                    if new_joltage[joltage_index] >= count {
+                        new_joltage[joltage_index] -= count;
+                    } else {
+                        has_button = false;
+                        break 'buttons;
+                    }
+                }
+            }
+            if has_button {
+                let try_result = dfs2(&new_joltage, new_buttons_allowed, buttons);
+                if try_result != u32::MAX {
+                    result = result.min(lowfreq + try_result);
+                }
+            }
+
+            let button_index = counts.iter().rposition(|&count| count != 0).unwrap();
+            if button_index == 0 {
+                break; //No more buttons to try.
+            }
+            let count = counts[button_index];
+            counts[button_index - 1] += 1;
+            counts[button_index] = 0;
+            counts[matching_buttons.len() - 1] = count - 1;
         }
     }
 
-    let key = (state.clone(), path_len);
-    if let Some(c) = cache.get(&key) {
-        return *c;
-    }
-
-    let mut result = i64::MAX - 1;
-    for i in 0..machine.buttons.len() {
-        for &k in &machine.buttons[i] {
-            state[k] += 1;
-        }
-        let r = 1 + dfs2(state, machine, i + 1, cache);
-        if r > 0 {
-            result = result.min(r);
-        }
-        //Restore.
-        for &k in &machine.buttons[i] {
-            state[k] -= 1;
-        }
-    }
-
-    cache.insert(key, result);
     result
 }
 
-pub fn part2(input: String) -> i64 {
+pub fn part2(input: String) -> u32 {
     let machines = parse(input);
     machines.into_par_iter().map(|mut machine| {
-        println!("Machine joltages {:?}", machine.joltages);
         //Sort the buttons by how commonly they affect each joltage.
         let mut joltage_affected_count = vec![0; machine.joltages.len()];
         for button in &machine.buttons {
@@ -144,8 +170,7 @@ pub fn part2(input: String) -> i64 {
         }
         sort_indices.sort_unstable_by_key(|i| button_damage[*i]);
         machine.buttons = sort_indices.iter().map(|&i| machine.buttons[i].clone()).collect();
-
-        let mut state = vec![0; machine.joltages.len()];
-        dfs2(&mut state, &machine, 0, &mut HashMap::new())
-    }).sum::<i64>()
+        let result = dfs2(&machine.joltages, (1 << machine.buttons.len()) - 1, &machine.buttons);
+        result
+    }).sum::<u32>()
 }
